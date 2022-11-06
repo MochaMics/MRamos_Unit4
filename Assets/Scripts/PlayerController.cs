e7b9c181e83a432c93af96ff8d9f17516b1f06c5
MochaMics <Mico.o.Ramos@gmail.com>
2022-11-05 18:52:10 -0700

    MRamos_Unit4_LessonD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     public GameObject powerUpRing;
 
     bool hasPowerUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
